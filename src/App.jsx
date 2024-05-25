8ad4dd9c149f4faa1cca788bafa6310593b341d5
rodnoika <.email@example.com>
2024-05-25 11:23:39 +0500

    test

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GameGenerator from './components/GameGenerator';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <GameGenerator />
+    </div>
+  );
+};
+
+export default App;
